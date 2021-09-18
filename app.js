ad262fb68e8c4fcba8c4d80ba5f0b86bebc4d361
brannonstarnes <brannlee808@gmail.com>
2021-09-17 22:14:26 -0700

    added congrats message and fixed game counters

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //Welcome the User and get their name
 let userName = prompt("Hello, and welcome to my Game! What is your name?");
-
+let correctTotal = 0;
 //*************************QUIZ ABOUT ME GAME********************************
 
 // //Ask the first yes/no question while using their name
@@ -14,71 +14,80 @@ let born = prompt(
 //Allow user to answer yes/no OR y/n. Take input and make lowercase
 
 //Question 1
-function meQuiz(){
-if (born.toLowerCase() == "yes" || born.toLowerCase() == "y") {
-  alert("Ooh, sorry, " + userName + ", that is incorrect.");
-} else if (born.toLowerCase() == "no" || born.toLowerCase() == "n") {
-  alert("Ding Ding Ding! Way to go, " + userName + "!");
-} else {
-  alert("Please answer yes or no only.");
-}
+function meQuiz() {
+  if (born.toLowerCase() == "yes" || born.toLowerCase() == "y") {
+    alert("Ooh, sorry, " + userName + ", that is incorrect.");
+  } else if (born.toLowerCase() == "no" || born.toLowerCase() == "n") {
+    alert("Ding Ding Ding! Way to go, " + userName + "!");
+    correctTotal++;
+  } else {
+    alert("Please answer yes or no only.");
+  }
 
-//Question 2
-let school = prompt(
-  "Next question: Did Brannon graduate from Harvard Law? Yes/No?",
-  ""
-);
-//Allow yes/no or y/n as above
-if (school.toLowerCase() == "yes" || school.toLowerCase() == "y") {
-  alert("Ooh, sorry, " + userName + ", that is incorrect.");
-} else if (school.toLowerCase() == "no" || school.toLowerCase() == "n") {
-  alert("Well that was an easy one, right, " + userName + "?");
-} else {
-  alert("Please answer yes or no only.");
-  // return;
-}
+  //Question 2
+  let school = prompt(
+    "Next question: Did Brannon graduate from Harvard Law? Yes/No?",
+    ""
+  );
+  //Allow yes/no or y/n as above
+  if (school.toLowerCase() == "yes" || school.toLowerCase() == "y") {
+    alert("Ooh, sorry, " + userName + ", that is incorrect.");
+  } else if (school.toLowerCase() == "no" || school.toLowerCase() == "n") {
+    alert("Well that was an easy one, right, " + userName + "?");
+    correctTotal++;
+  } else {
+    alert("Please answer yes or no only.");
+    // return;
+  }
 
-//Question 3
-let grewUp = prompt("Third question: Did Brannon grow up in Tennessee? Yes/No");
+  //Question 3
+  let grewUp = prompt(
+    "Third question: Did Brannon grow up in Tennessee? Yes/No"
+  );
 
-if (grewUp.toLowerCase() == "yes" || grewUp.toLowerCase() == "y") {
-  //console.log('That's correct!')
-  alert("Well done, " + userName + "! That is correct.");
-} else if (grewUp.toLowerCase() == "no" || grewUp.toLowerCase() == "n") {
-  //console.log('That's incorrect!')
-  alert("Swing and a miss! Sorry, " + userName + "!");
-} else {
-  alert("Please answer yes or no only.");
-  // return;
-}
+  if (grewUp.toLowerCase() == "yes" || grewUp.toLowerCase() == "y") {
+    //console.log('That's correct!')
+    alert("Well done, " + userName + "! That is correct.");
+    correctTotal++;
+  } else if (grewUp.toLowerCase() == "no" || grewUp.toLowerCase() == "n") {
+    //console.log('That's incorrect!')
+    alert("Swing and a miss! Sorry, " + userName + "!");
+  } else {
+    alert("Please answer yes or no only.");
+    // return;
+  }
 
-//Question 4
-let nurseQ = prompt(
-  "Question 4: Did Brannon once aspire to be a nurse? Yes/No"
-);
+  //Question 4
+  let nurseQ = prompt(
+    "Question 4: Did Brannon once aspire to be a nurse? Yes/No"
+  );
 
-if (nurseQ.toLowerCase() == "yes" || nurseQ.toLowerCase() == "y") {
-  alert("Niiiice, " + userName + ", that is correct!");
-} else if (nurseQ.toLowerCase() == "no" || nurseQ.toLowerCase() == "n") {
-  alert("Oooh, sorry, " + userName + "!");
-} else {
-  alert("Please answer yes or no only.");
-  // return;
-}
+  if (nurseQ.toLowerCase() == "yes" || nurseQ.toLowerCase() == "y") {
+    alert("Niiiice, " + userName + ", that is correct!");
+    correctTotal++;
+  } else if (nurseQ.toLowerCase() == "no" || nurseQ.toLowerCase() == "n") {
+    alert("Oooh, sorry, " + userName + "!");
+  } else {
+    alert("Please answer yes or no only.");
+    // return;
+  }
 
-//Question 5
-let goal = prompt(
-  "Last question: Does Brannon want to master the Terminal? Yes/No"
-);
+  //Question 5
+  let goal = prompt(
+    "Last question: Does Brannon want to master the Terminal? Yes/No"
+  );
 
-if (goal.toLowerCase() == "yes" || goal.toLowerCase() == "y") {
-  alert("Nice work, " + userName + "! You nailed it!");
-} else if (goal.toLowerCase() == "no" || goal.toLowerCase() == "n") {
-  alert("WROOOONG! Sorry, " + userName + ", sometimes I get carried away. :/");
-} else {
-  alert("Please answer yes or no only.");
-  // return;
-}
+  if (goal.toLowerCase() == "yes" || goal.toLowerCase() == "y") {
+    alert("Nice work, " + userName + "! You nailed it!");
+    correctTotal++;
+  } else if (goal.toLowerCase() == "no" || goal.toLowerCase() == "n") {
+    alert(
+      "WROOOONG! Sorry, " + userName + ", sometimes I get carried away. :/"
+    );
+  } else {
+    alert("Please answer yes or no only.");
+    // return;
+  }
 }
 meQuiz();
 
@@ -105,6 +114,13 @@ function numberGame() {
       alert("That was your 4th attempt. Game Over!");
       break;
     }
+    if (numberGuess == goalNumber) {
+      alert(
+        "Congratualtions! You got it in " + guessCount.toString() + " tries!"
+      );
+      correctTotal++;
+      break;
+    }
     if (numberGuess > goalNumber) {
       console.log("inside high", numberGuess);
       alert("Your guess is too high.");
@@ -113,74 +129,72 @@ function numberGame() {
       console.log("inside low", numberGuess);
       alert("Your guess is too low.");
     }
-    if (numberGuess == goalNumber) {
-      guessCount++;
-      console.log("inside correct", numberGuess);
 
-      alert(
-        "Congratualtions! You got it in " + guessCount.toString() + " tries!"
-      );
-      break;
-    }
     if (!numberGuess) {
       alert("Please enter a valid number.");
     }
-    //guessCount++;
     numberGuess = prompt("That's " + guessCount + " guesses. Try again!");
-    console.log("Inside try again", numberGuess);
   }
 }
 
 numberGame();
 // //tryAgain();
 
- const myStates = [
-   "tennessee",
-   "utah",
-   "florida",
-   "illinois",
-   "texas",
-   "georgia",
- ];
+const myStates = [
+  "tennessee",
+  "utah",
+  "florida",
+  "illinois",
+  "texas",
+  "georgia",
+];
 
- let guessState = prompt(
-   "One more game. Can you guess one of six states I have lived in?"
- );
- let stateGuessCount = 1;
- let correctAns = false;
+let guessState = prompt(
+  "One more game. Can you guess one of six states I have lived in?"
+);
+let stateGuessCount = 0;
+let correctAns = false;
 
 function stateGuessGame() {
   // if (guessState == true){
 
   while (stateGuessCount < 6 && correctAns == false) {
-
     for (var i = 0; i < myStates.length; i++) {
       //credit https://www.geeksforgeeks.org/ways-iterating-array-javascript/
 
       if (guessState.toLowerCase() == myStates[i]) {
         //congrats
+        stateGuessCount++;
         console.log("inside state guess win", guessState);
-         alert(
-           "Congratulations, " +
-             userName +
-             " you got it right in " +
-             stateGuessCount +
-             " guesses!"
-         );
+        alert(
+          "Congratulations, " +
+            userName +
+            " you got it right in " +
+            stateGuessCount +
+            " guesses!"
+        );
+        correctTotal++;
         correctAns = true;
         break;
       }
     }
     {
       //go again
-      stateGuessCount++
+      stateGuessCount++;
       console.log("inside guess again", i);
       //
     }
-    if (!correctAns){
-    guessState = prompt("That's " + stateGuessCount + " guess. Try again.");
-    console.log("stateguess number", stateGuessCount);
+    if (!correctAns) {
+      guessState = prompt("That's " + stateGuessCount + " guess. Try again.");
+      console.log("stateguess number", stateGuessCount);
     }
-  } 
+  }
 }
 stateGuessGame();
+alert(
+  "Thanks for playing, " +
+    userName +
+    "! You scored " +
+    correctTotal +
+    " correct answers! Way to go!"
+);
